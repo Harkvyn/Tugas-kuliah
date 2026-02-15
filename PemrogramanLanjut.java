457629a4c722466565ee319e20323c4c8a47d2a7
Pecel Antartika <hamamyusufa@gmail.com>
2026-02-15 16:54:09 +0700

    Refactor classes and methods for shape calculations

--- PemrogramanLanjut.java
+++ PemrogramanLanjut.java
@@ -1,15 +1,58 @@
 import java.util.*;
 
+public class App {
+    public static void main(String[] args) {
+    bujurSangkar bujursangkar = new bujurSangkar(3);
+        bujursangkar.info();
+        System.out.println("Luas bujur sangkar: " + bujursangkar.luas());
+
+        lingkaran Lingkaran = new lingkaran(3);
+        Lingkaran.info();
+        System.out.println("Luas lingkaran" + Lingkaran.luas());    
+        
+    }
+}
+
+
+
 class bidangDuaDimensi {
-    String nama;
+    private String nama;
     
     bidangDuaDimensi(String nama) {
         this.nama = nama;
     }
     
-    // method info
+    void info() {
+        System.out.println(this.nama);
+    }
     
-    // method luas
+        int luas();
     
     
 }
+
+    class bujurSangkar extends bidangDuaDimensi{
+        private int sisi;
+
+        bujurSangkar(int sisi) {
+            super("Bujur Sangkar")
+            this.sisi = sisi;
+        }
+
+        int luas() {
+            return sisi * sisi;
+        }
+    }
+
+    class lingkaran extends bidangDuaDimensi{
+        private int jari;
+
+        lingkaran(int jari) {
+            super("Lingkaran");
+            this.jari = jari;
+        }
+        int luas() {
+        return 3.14 * jari * jari;
+        }
+        
+    }
