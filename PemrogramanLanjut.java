ac5135179d26b5472f891a72a795b65d0f7798da
Pecel Antartika <hamamyusufa@gmail.com>
2026-02-15 12:44:02 +0700

    Create bidangDuaDimensi class with name attribute
    Define a class for two-dimensional shapes with a name attribute.

---
+++ PemrogramanLanjut.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class bidangDuaDimensi {
+    String nama;
+    
+    bidangDuaDimensi(String nama) {
+        this.nama = nama;
+    }
+    
+    // method info
+    
+    // method luas
+    
+    
+}
