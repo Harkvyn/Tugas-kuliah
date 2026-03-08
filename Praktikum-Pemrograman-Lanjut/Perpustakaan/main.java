2610ede4df1aa565b438909816b3775f981a6ddb
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-08 10:27:43 +0700

    Add kategori class and category arrays in main

--- Praktikum-Pemrograman-Lanjut/Perpustakaan/main.java
+++ Praktikum-Pemrograman-Lanjut/Perpustakaan/main.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+class kategori {
+  String nama;
+  int ID;
+
+  kategori(String nama, int ID) {
+    this.nama = nama;
+    this.ID = ID;
+  }
+}
+
+class main {
+  kategori[] teknologi = new kategori[5];
+  kategori[] filsafat = new kategori[5];
+  kategori[] sejarah = new kategori[5];
+  kategori[] agama = new kategori[5];
+  kategori[] psikologi = new kategori[5];
+  kategori[] politik = new kategori[5];
+  kategori[] fiksi = new kategori[5];
+}
