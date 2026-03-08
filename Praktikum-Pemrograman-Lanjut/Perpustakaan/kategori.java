bd4d01de6705e79c426ee299b480e7bc85e03f8f
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-08 10:54:47 +0700

    Add kategori and buku classes with constructors

--- Praktikum-Pemrograman-Lanjut/Perpustakaan/kategori.java
+++ Praktikum-Pemrograman-Lanjut/Perpustakaan/kategori.java
@@ -1,1 +1,23 @@
+package Perpustakaan;
+import java.util.*;
 
+class kategori {
+  String nama;
+  int ID;
+
+  kategori(String nama, int ID) {
+    this.nama = nama;
+    this.ID = ID;
+  }
+}
+
+class buku {
+  String judul;
+  int ID;
+  String kategori;
+  buku(String judul, int ID, String kategori) {
+    this.judul = judul;
+    this.ID = ID;
+    this.kategori = kategori;
+  }
+}
